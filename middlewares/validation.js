a27055ee7a44c83ecfba302716e0d15bd8cea4e1
Sacha-Marciano <Momarciano26@gmail.com>
2024-10-27 22:46:23 +0200

    v3.2.3 Fix after failed GitHub Actions

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -26,6 +26,7 @@ module.exports.validateCreateItem = celebrate({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'the "imageUrl" field must be a valid url',
     }),
+    weather: Joi.string.required(),
   }),
 });
 
